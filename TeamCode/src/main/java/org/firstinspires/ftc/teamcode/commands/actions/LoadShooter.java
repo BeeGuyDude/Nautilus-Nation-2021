4179980d2d5d54556489b1233a610437a6aae737
Cian Capacci <46614295+BeeGuyDude@users.noreply.github.com>
2021-04-02 12:53:14 -0600

    LoadShooter runs intake and storage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/actions/LoadShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/actions/LoadShooter.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.commands.actions;
 
 import org.firstinspires.ftc.teamcode.commands.basecommands.Command;
+import org.firstinspires.ftc.teamcode.mechanisms.Intake;
 import org.firstinspires.ftc.teamcode.mechanisms.Storage;
 import org.firstinspires.ftc.teamcode.mechanisms.mechanismhandlers.MechanismEngine;
 
@@ -8,9 +9,11 @@ import static org.firstinspires.ftc.teamcode.framework.Constants.*;
 
 public class LoadShooter extends Command {
     private Storage localStorage = MechanismEngine.getInstance().getMechanism(Storage.class);
+    private Intake localIntake = MechanismEngine.getInstance().getMechanism(Intake.class);
 
     public LoadShooter() {
         Requires(localStorage);
+        Requires(localIntake);
     }
 
     public void initialize() {
@@ -19,6 +22,7 @@ public class LoadShooter extends Command {
 
     public void execute() {
         localStorage.runStorage(STORAGE_FEED_SPEED);
+        localIntake.runIntake(STORAGE_FEED_SPEED);
     }
 
     public boolean isFinished() {
@@ -27,5 +31,6 @@ public class LoadShooter extends Command {
 
     public void end() {
         localStorage.runStorage(0);
+        localIntake.runIntake(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Storage.java
@@ -1,19 +1,25 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.framework.util.TelemetryHandler;
 import org.firstinspires.ftc.teamcode.mechanisms.devicehandlers.DCMotorHandler;
 import org.firstinspires.ftc.teamcode.mechanisms.mechanismhandlers.Mechanism;
 
 public class Storage extends Mechanism {
 
     private DCMotorHandler storageMotor = new DCMotorHandler("storage_motor", true, true);
+    private DistanceSensor storageSensor;
 
     public void init(HardwareMap hwmap) {
         storageMotor.init(hwmap);
+        storageSensor = hwmap.get(DistanceSensor.class, "storage_distance_sensor");
     }
 
     public void runStorage(double power) {
         storageMotor.setPower(power);
+        TelemetryHandler.getInstance().getTelemetry().addData("Distance Sensor Value", storageSensor.getDistance(DistanceUnit.CM));
     }
 }
